f688fcd07c938222fbfcecff4e09c4b882144598
Raheleh Yoosefzadeh <raheleh.yoosefzadeh@arpm.co>
2023-03-28 21:16:35 +0330

    updated main plugin file

--- inpsyde-plugin.php
+++ inpsyde-plugin.php
@@ -4,7 +4,7 @@ namespace MyTask;
 
 /**
  * Plugin Name: Inpsyde Plugin
- * Plugin URI:  https://github.com/rahelehyoosefzadeh/inpsydetask/
+ * Plugin URI:  https://github.com/rahelehyoosefzadeh/inpsyde/
  * Description: This plugin  displays lovely users table as a react component in an endpoint
  *              and displays the details for one selected user at a time - that's my Inpsyde task.
  * Author:      Raheleh Yoosefzadeh
